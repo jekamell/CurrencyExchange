aedd079ab870235828de493c4869fb09d71ac7b5
Evgeniy Malykhin <e.malykhin@richmedia.systems>
2017-04-08 23:45:58 +0300

    add DI configuration

---
+++ src/AppBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ * @package AppBundle\DependencyInjection
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('app');
+
+        return $treeBuilder;
+    }
+}
